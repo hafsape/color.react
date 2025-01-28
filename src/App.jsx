50bb6c23c714211cb1c42477bab35baf636d6377
HafsaP@24 <hafsape04@gmail.com>
2025-01-28 16:45:25 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,19 @@ import './App.css'
 
 function App() {
  
-   const [clr,setClr]=useState("#ffff")
+   const [clr,setClr]=useState("black")
    
   return (
     <>
     
-    <div  style={{height:"100vh",backgroundColor:clr} }className='d-flex flex-column align-items-center justify-content-center'>
+    <div  style={{height:"100vh",backgroundColor:clr} }className='d-flex flex-column text-light align-items-center justify-content-center'>
       <h1>Background Changing App</h1>
-      <div className='d-flex  align-items-center justify-content-center' >
+      <div style={{backgroundColor:"white",height:"50px",borderRadius:"20px"}} className='d-flex mt-5  align-items-center justify-content-center' >
          <button onClick={()=>setClr("red")} className='btn btn-danger' >red</button>
          <button onClick={()=>setClr("orange")} className='btn btn-warning' >orange</button>
          <button onClick={()=>setClr("blue")} className='btn btn-info'>blue</button>
          <button onClick={()=>setClr("green")} className='btn btn-success'>green</button>
-         <button onClick={()=>setClr("#ffff")} className='btn btn-light'>cancel</button>
+         <button onClick={()=>setClr("grey")} className='btn btn-secondary'>grey</button>
       </div>
     </div>
     </>
